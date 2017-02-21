a9229cc1da6ef739771ca2dbec97a0b536f39bcc
Stanislav <tigrarion@gmail.com>
2017-02-22 00:22:10 +0200

    result6

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -14,6 +14,41 @@ import { LoginService } from '../login/login.service';
 })
 export class SidebarComponent implements OnInit {
   public authRole: string;
+  public admins = [
+      {route: 'osbb' , name: 'osbb' , class: 'fa fa-home fa-fw fa-4x'},
+      {route: 'houses' , name: 'houses' , class: 'fa fa-user'},
+      {route: 'apartments' , name: 'rents' , class: 'fa fa-building-o'},
+      {route: 'ticket' , name: 'tickets' , class: 'fa fa-ticket'},
+      {route: 'calendar' , name: 'calendar' , class: 'fa fa-calendar'},
+      {route: 'events' , name: 'events' , class: 'fa fa-comments'},
+      {route: 'attachments' , name: 'attachments' , class: 'fa fa-cloud-download'},
+      {route: 'users' , name: 'users' , class: 'fa fa-users'},
+      {route: 'role' , name: 'roles' , class: 'fa fa-building-o'}
+    ];
+  public managers = [
+      {route: 'houses' , name: 'houses' , class: 'fa fa-user'},
+      {route: 'apartments' , name: 'rents' , class: 'fa fa-building-o'},
+      {route: 'ticket' , name: 'tickets' , class: 'fa fa-ticket'},
+      {route: 'calendar' , name: 'calendar' , class: 'fa fa-calendar'},
+      {route: 'events' , name: 'events' , class: 'fa fa-comments'},
+      {route: 'osbb/bill' , name: 'bills_osbb' , class: 'fa fa-credit-card-alt'},
+      {route: 'provider' , name: 'providers' , class: 'fa fa-building'},
+      {route: 'contract' , name: 'contracts' , class: 'fa fa-bookmark'}
+    ];
+  public users = [
+      {route: 'ticket' , name: 'tickets' , class: 'fa fa-ticket'},
+      {route: 'calendar' , name: 'calendar' , class: 'fa fa-calendar'},
+      {route: 'events' , name: 'events' , class: 'fa fa-comments'},
+      {route: 'provider/info' , name: 'providers' , class: 'fa fa-building-o'},
+      {route: 'report' , name: 'reports' , class: 'fa fa-list-alt'}
+    ];
+  public providers = [
+      {route: 'rents/electricity' , name: 'electricity'},
+      {route: 'rents/gas' , name: 'gas'},
+      {route: 'rents/water' , name: 'water'},
+      {route: 'rents/service' , name: 'debt'},
+      {route: 'bill/parentbillid' , name: 'rate'},
+  ];
   constructor(
     public loginService: LoginService
   ) {}

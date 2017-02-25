ec2755e555efa2937560bad10123d7c1b85a5cb2
Stanislav <tigrarion@gmail.com>
2017-02-25 12:40:27 +0200

    constants3

--- src/app/shared/login/login.constants.ts
+++ src/app/shared/login/login.constants.ts
@@ -1,14 +1,14 @@
 export class LoginConstants {
   public static get Login(){
-  	return {
-  		serverUrl: 'http://localhost:8080/myosbb',
-  		auth: 'Authorization',
-  		contentType: 'Content-Type',
-  		appJson: 'application/json',
-  		appXwfu: 'application/x-www-form-urlencoded',
-  		accept: 'Accept',
-  		basicToken: 'Basic  Y2xpZW50YXBwOjEyMzQ1Ng==',
-  		bearer: 'Bearer'
-  	}
+    return {
+      serverUrl: 'http://localhost:8080/myosbb',
+      auth: 'Authorization',
+      contentType: 'Content-Type',
+      appJson: 'application/json',
+      appXwfu: 'application/x-www-form-urlencoded',
+      accept: 'Accept',
+      basicToken: 'Basic  Y2xpZW50YXBwOjEyMzQ1Ng==',
+      bearer: 'Bearer'
+    }
   }
 }
